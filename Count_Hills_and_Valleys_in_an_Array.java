40c9167bc156575d095b681f7e142e1fa5d9b9e9
Sukant Saumya <88524348+sukantsaumya@users.noreply.github.com>
2025-01-01 22:37:52 +0530

    Update Count_Hills_and_Valleys_in_an_Array.java

--- Count_Hills_and_Valleys_in_an_Array.java
+++ Count_Hills_and_Valleys_in_an_Array.java
@@ -1,5 +1,16 @@
-  class Solution {
-    public int countHillValley(int[] nums) {
-        
+public int countHillsAndValleys(int[] array) {
+    int countHills = 0;
+    int countValleys = 0;
+    
+    for (int i = 0; i < array.length; i++) {
+       
+        if (array[i] > array[i + 1]) {
+            countHills++;
+        } 
+     
+        else if (array[i] < array[i + 1]) {
+            countValleys++;
+        }
     }
+    return countHills + countValleys;
 }
